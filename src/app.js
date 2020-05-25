1c2e2b86fcee080849d7bd4e8cfad84863bd6df8
w-MAX-w <mxprojekt@gmail.com>
2020-05-26 00:10:00 +0300

    Продолжение проекта, подключение базы данных через Firebase, добавление отображения списка заданных вопросов

--- src/app.js
+++ src/app.js
@@ -1,3 +1,34 @@
+import { Question } from './question'
+import { isValid } from './utils'
 import './styles.css'
 
-console.log('App working...')
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+window.addEventListener('load', Question.renderList)
+form.addEventListener('submit', submitFormHandler)
+input.addEventListener('input', () => {
+    submitBtn.disabled = !isValid(input.value)
+})
+
+function submitFormHandler(event) {
+    event.preventDefault()
+
+    if (isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+
+        submitBtn.disabled = true
+        // Async request to server to save question
+
+        Question.create(question).then(() => {
+            input.value = ''
+            input.className = ''
+            submitBtn.disabled = false
+        })
+    }
+}
+
