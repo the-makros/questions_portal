1a3908daeae017223d55a7ce9f25c9a1744af5da
the-makros <mxprojekt@gmail.com>
2020-05-26 17:26:29 +0300

    Конечная сборка приложения, настройка авторизации

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import { Question } from './question'
-import { isValid } from './utils'
+import { isValid, createModal } from './utils'
 import './styles.css'
+import { getAuthForm, authWithEmailAndPassword } from './auth'
 
 const form = document.getElementById('form')
 const input = form.querySelector('#question-input')
 const submitBtn = form.querySelector('#submit')
+const modalBtn = document.getElementById('modal-btn')
 
 window.addEventListener('load', Question.renderList)
 form.addEventListener('submit', submitFormHandler)
+modalBtn.addEventListener('click', openModal)
 input.addEventListener('input', () => {
     submitBtn.disabled = !isValid(input.value)
 })
@@ -32,3 +35,29 @@ function submitFormHandler(event) {
     }
 }
 
+function openModal() {
+    createModal('Авторизация', getAuthForm())
+    document.getElementById('auth-form').addEventListener('submit', authFormHandler, {once: true})
+}
+
+function authFormHandler(event) {
+    event.preventDefault()
+
+    const btn = event.target.querySelector('button')
+    const email = event.target.querySelector('#email').value
+    const password = event.target.querySelector('#password').value
+
+    authWithEmailAndPassword(email, password)
+        .then(Question.fetch)
+        .then(renderModalAfterAuth)
+        .then(() => btn.disabled = false)
+}
+
+function renderModalAfterAuth(content) {
+    if (typeof content === 'string') {
+        createModal('Ошибка!', content)
+    } else {
+        createModal('Список вопросов', Question.listToHTML(content))
+    }
+    
+}
--- src/question.js
+++ src/question.js
@@ -16,6 +16,24 @@ export class Question {
         .then(Question.renderList)
     }
 
+    static fetch(token) {
+        if (!token) {
+            return Promise.resolve('<p class="error">У вас нет токена</p>')
+        }
+        return fetch(`https://question-portal.firebaseio.com/questions.json?auth=${token}`)
+            .then(response => response.json())
+            .then(response => {
+                if (response && response.error) {
+                    return `<p class="error">${response.error}</p>`
+                }
+
+                return response ? Object.keys(response).map(key => ({
+                    ...response[key],
+                    id: key
+                })) : []
+            })
+    }
+
     static renderList() {
         const questions = getQuestionsFromLocalStorage()
 
@@ -26,6 +44,12 @@ export class Question {
         const list = document.getElementById('list')
 
         list.innerHTML = html
+    }
+
+    static listToHTML(questions) {
+        return questions.length
+        ? `<ol>${questions.map(q => `<li>${q.text}</li>`).join(' ')}</ol>`
+        : '<p>Вопросов пока нет</p>'
     }
 }
 
